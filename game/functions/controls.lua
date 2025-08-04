77954d0ce3abc95e9d4561872d2e20a1e161a2d2
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2025-08-04 14:03:50 +0200

    Fix Pause button

--- game/functions/controls.lua
+++ game/functions/controls.lua
@@ -1,38 +1,37 @@
-function ctrl_restart()
-    -- RESTART WITH "ESCAPE" (supposedly pressed by player 1)
-    if love.keyboard.isDown("escape") then
-        start_game(1)
-    end
-    -- RESTART WITH "ENTER" (supposedly pressed player 2)
-    if love.keyboard.isDown("return") then
-        start_game(2)
-    end
-end
-
-function ctrl_pause()
-    -- Pause with "Space" which is easily accessible by both players
-    if love.keyboard.isDown("space") then
-        pause_game = not pause_game
-    end
+function love.keypressed(key, scancode, isrepeat)
+	-- Pause with "Space" which is easily accessible by both players
+	if key == "space" then
+		pause_game = not pause_game
+	elseif not pause_game then
+		-- Restart with "Escape" (supposedly pressed by player 1)
+		if key == "escape" then
+			start_game(1)
+		-- Restart with "Enter" (supposedly pressed by player 2)
+		elseif key == "return" then
+			start_game(2)
+		end
+	end
 end
 
 function ctrl_player1()
-    -- "E" for up and "C" for down, because they are at the same place on both QWERTY and AZERTY keyboards
-    if love.keyboard.isDown("e") and pad1.y > screen_padding then
-        pad1.y = pad1.y - pad_speed
-    end
-    if love.keyboard.isDown("c") and pad1.y < love.graphics.getHeight() - screen_padding - pad1.height then
-        pad1.y = pad1.y + pad_speed
-    end
+	-- "E" for up and "C" for down, because they are at the same place on both 
+	-- QWERTY and AZERTY keyboards
+	if love.keyboard.isDown("e") and pad1.y > screen_padding then
+		pad1.y = pad1.y - pad_speed
+	end
+	if love.keyboard.isDown("c") and
+	pad1.y < love.graphics.getHeight() - screen_padding - pad1.height then
+		pad1.y = pad1.y + pad_speed
+	end
 end
 
 function ctrl_player2()
-    -- "Up arrow" and "Down arrow"
-    if love.keyboard.isDown("up") and pad2.y > screen_padding then
-        pad2.y = pad2.y - pad_speed
-    end
-    if love.keyboard.isDown("down") and pad2.y < love.graphics.getHeight() - screen_padding - pad2.height then
-        pad2.y = pad2.y + pad_speed
-    end
+	-- "Up arrow" and "Down arrow"
+	if love.keyboard.isDown("up") and pad2.y > screen_padding then
+		pad2.y = pad2.y - pad_speed
+	end
+	if love.keyboard.isDown("down") and
+	pad2.y < love.graphics.getHeight() - screen_padding - pad2.height then
+		pad2.y = pad2.y + pad_speed
+	end
 end
-
--- game/main.lua
+++ game/main.lua
@@ -4,37 +4,33 @@ require("functions/move_ball")
 require("functions/graphics")
 
 function love.load()
-    initialize_window()
-    load_resources_in()
-    initialize_data()
+	initialize_window()
+	load_resources_in()
+	initialize_data()
 end
 
 function love.update(dt)
-    ctrl_pause()
+	if pause_game then
+		return
+	end
 
-    if pause_game then
-        return
-    end
-
-    ctrl_restart()
-    ctrl_player1()
-    ctrl_player2()
-    move_ball(dt)
+	ctrl_player1()
+	ctrl_player2()
+	move_ball(dt)
 end
 
 function love.draw()
-    draw_screen_padding()
-    draw_corners()
+	draw_screen_padding()
+	draw_corners()
 
-    if pause_game then
-        draw_commands()
-        return
-    end
+	if pause_game then
+		draw_commands()
+		return
+	end
 
-    draw_middle_line()
-    draw_score()
-    draw_pads()
-    draw_ball()
-    draw_ball_trail()
+	draw_middle_line()
+	draw_score()
+	draw_pads()
+	draw_ball()
+	draw_ball_trail()
 end
-
